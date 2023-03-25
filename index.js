867657ff421f15c4744e8305d5caa4b36d6380b5
Sam <sam.camhi@gmail.com>
2023-03-25 15:54:29 -0400

    Passed Functions Continued

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(act = "roller-skate") {
+    return `This Saturday, I want to ${act}!`;
+  }
+
+  function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+  }
+
+function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
